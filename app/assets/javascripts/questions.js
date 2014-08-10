ff75e857ea4ee577a9b1206eb45ac7cea47dba45
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-08-10 10:32:57 +0100

    first commit

---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,232 @@
+
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+var data = {
+    "Questions": [
+        {
+            "Q": "What is the Best Market to expand into from the UK?",
+            "Answers": [
+                "North America",
+                "China",
+                "Africa"
+            ]
+        },
+        {
+            "Q": "What is the most important factor in Choosing a Market?",
+            "Answers": [
+                "Customer Demand",
+                "Competitors (in the market)",
+                "Infrastructure"
+            ]
+        },
+        {
+            "Q": "What is the biggest challenge to entering a new market?",
+            "Answers": [
+                "Finances",
+                "Regulation/Legal",
+                "People./Culture"
+            ]
+        },
+        {
+            "Q": "Where are you in your entrepreneurship journey?",
+            "Answers": [
+                "At the starting block",
+                "Just getting warmed up",
+                "Really hitting stride",
+                "Crossing the finish line",
+                "Sponsoring someones race",
+                "Cheering from the sidelines"
+            ]
+        },
+        {
+            "Q": "What platform do you prefer?",
+            "Answers": [
+                "iOS (Apple)",
+                "Android",
+                "Windows"
+            ]
+        },
+        {
+            "Q": "What is the challenge to building a mobile product?",
+            "Answers": [
+                "Speed to Market",
+                "Cost",
+                "Skills/Resources"
+            ]
+        },
+        {
+            "Q": "What is the challenge in hiring in any niche area?",
+            "Answers": [
+                "Lack of Specific Skills",
+                "Depth of Talent Pool",
+                "Lack of Experience"
+            ]
+        },
+        {
+            "Q": "What is the key factor in choosing to hire someone?",
+            "Answers": [
+                "Personality",
+                "Experience",
+                "Correct Skill Set"
+            ]
+        },
+        {
+            "Q": "What do you think maintains an Entrepreneur?",
+            "Answers": [
+                "Passion",
+                "Money",
+                "Change the Status Quo"
+            ]
+        },
+        {
+            "Q": "What is the essential skill for an Entrepreneur?",
+            "Answers": [
+                "Communication",
+                "Negotiation",
+                "Financial Acumen"
+            ]
+        },
+        {
+            "Q": "What tech area will have the most influence in 10 years?",
+            "Answers": [
+                "Medical",
+                "Media",
+                "Financial"
+            ]
+        },
+        {
+            "Q": "Do you think we should expand onto other planets?",
+            "Answers": [
+                "Yes",
+                "No"
+            ]
+        },
+        {
+            "Q": "Should governments fund large tech projects?",
+            "Answers": [
+                "Yes",
+                "No"
+            ]
+        },
+        {
+            "Q": "What should be priority for Funding in the future?",
+            "Answers": [
+                "Health",
+                "Energy",
+                "Food/Natural Resources"
+            ]
+        },
+        {
+            "Q": "Will the old media entities adapt quick enough?",
+            "Answers": [
+                "Yes",
+                "No"
+            ]
+        },
+        {
+            "Q": "What do you think, is the best money model for content?",
+            "Answers": [
+                "Pay as You Consume",
+                "Subscription",
+                "Advertising"
+            ]
+        },
+        {
+            "Q": "Is content really king? ",
+            "Answers": [
+                "Yes",
+                "No"
+            ]
+        },
+        {
+            "Q": "Where do you primarily go to consume news content?",
+            "Answers": [
+                "TV/Radio",
+                "Mobile/Tablet",
+                "Print/Magazine"
+            ]
+        },
+        {
+            "Q": "Why do you stick with a brand?",
+            "Answers": [
+                "Loyalty",
+                "Values",
+                "Consistency"
+            ]
+        },
+        {
+            "Q": "Are we heading towards a more focused/niche brand economy?",
+            "Answers": [
+                "Yes",
+                "No"
+            ]
+        }
+    ]
+}
+
+
+function displayresults(data){
+  var questions = data['Questions']
+  // console.log(main)
+  var main = data['Questions']
+  var mainarray = main.splice(0,1)
+  for (i=0; i < mainarray.length; i++){
+    // console.log(main[i].Q)
+    // console.log(currentobject)
+    console.log(mainarray)
+    var contentString = $('<div id="content">' + '<h3>' + main[i].Q + '</h3>');
+    contentString.appendTo(".displayquestions")
+  }
+}
+
+
+$(function(){
+
+  $('.togglequestions').click(function(){
+
+    console.log(data)
+
+    displayresults(data)
+
+
+   // when i make a najax request it somehow amkes a request to another page which has the data in a json formate 
+   // rails auotmatiacally has its in json formate
+   // so maybe i can see the questions data in ta rails table 
+   // then make a reuqestions to these questions 
+   // so table would be question - question description
+   // then awnsers, - aswners choices
+
+
+   // but think how i would be able to get the data with out rails 
+   // what would the request url be?????
+   // just make the request url the variable name???? 
+
+
+   // the totrial might have a way ti fugure it out
+
+   // ajax request but simply put the hash variable as the url 
+   // put the rueqest into a loop which iterates through each question 
+   // then grab the current question its on every time you click on it... 
+   // and display the content in p and h1 tags
+
+
+   // 9:909 on train - able to trigger so it shows all the objects
+   // but how can i do so it just displays the current element in the array and everytime you click through???
+
+   // USE BACKPACKERS ID TO DISPLAY QUESTION. in budgie backpackers. 
+   // 243 app see gerrys methid 
+   // javascript definintive guide 561, 563 
+
+   
+
+  })
+
+
+
+
+
+
+
+
+})
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Questionair::Application.config.secret_key_base = '1cb2a472b5869dd41bb3190fb16bb1dc86d7cfdb852815175929bbc5eff8c8e3ddce7f1e32841e2c68909c173b2bd9d64d6bc32339932b7150ea20ff43e2fae6'
